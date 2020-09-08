a068963d9794430ac5ff649535970bafb3bd8336
petros <pit.trak@gmail.com>
2020-09-08 13:37:00 +0300

    Generate spring project, added packages and edited application.properties

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-Spring boot TODO-Manager Application
+# todo-management-spring-boot
+
+
+# Mini Todo Management Project using Spring Boot + Spring MVC + Spring Security + JSP + Hibernate + MySQL
+
+- http://www.javaguides.net/2018/09/mini-todo-management-project-using-spring-boot-springmvc-springsecurity-jsp-hibernate-mysql.html
---
+++ src/main/java/com/petros/todo/todomanagement/TodoManagementSpringBoot2Application.java
@@ -0,0 +1,12 @@
+package com.petros.todo.todomanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoManagementSpringBoot2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoManagementSpringBoot2Application.class, args);
+	}
+}
