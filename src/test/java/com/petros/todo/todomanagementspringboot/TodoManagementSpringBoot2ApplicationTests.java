d9ea92b26ead3f81feb59fb815af85249e5c1189
petros <pit.trak@gmail.com>
2020-09-08 15:42:00 +0300

    Added @SpringBootTest(classes = TodoManagementSpringBoot2Application.class)

--- src/test/java/com/petros/todo/todomanagementspringboot/TodoManagementSpringBoot2ApplicationTests.java
+++ src/test/java/com/petros/todo/todomanagementspringboot/TodoManagementSpringBoot2ApplicationTests.java
@@ -1,12 +1,13 @@
-package net.guides.todo.todomanagementspringboot2;
+package com.petros.todo.todomanagementspringboot;
 
+import com.petros.todo.todomanagement.TodoManagementSpringBoot2Application;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = TodoManagementSpringBoot2Application.class)
 public class TodoManagementSpringBoot2ApplicationTests {
 
 	@Test
