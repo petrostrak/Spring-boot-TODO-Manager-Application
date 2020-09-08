e36fe7044a2930f571383b5fc165fe4c578e741b
petros <pit.trak@gmail.com>
2020-09-08 16:16:25 +0300

    Changed db name and updated SecurityConfig class

--- src/main/java/com/petros/todo/todomanagement/security/SecurityConfig.java
+++ src/main/java/com/petros/todo/todomanagement/security/SecurityConfig.java
@@ -8,9 +8,8 @@ package com.petros.todo.todomanagement.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.authentication.configuration.GlobalAuthenticationConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 /**
  *
@@ -18,12 +17,12 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
  */
 
 @Configuration
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
+public class SecurityConfig extends GlobalAuthenticationConfigurerAdapter {
     
     @Autowired
     public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception{
         auth.inMemoryAuthentication()
-                .passwordEncoder(NoOpPasswordEncoder.getInstance()).withUser("admin").password("admin").roles("USER", "ADMIN");
+                .withUser("admin").password("{noop}admin").roles("USER", "ADMIN");
     }
     
     protected void configure(HttpSecurity http) throws Exception {
