93123ebce927c414b47dc71200fc0ba174f6606d
petros <pit.trak@gmail.com>
2020-09-08 15:36:46 +0300

    Added SecurityConfig

---
+++ src/main/java/com/petros/todo/todomanagement/security/SecurityConfig.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.petros.todo.todomanagement.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+/**
+ *
+ * @author petros_trak
+ */
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    
+    @Autowired
+    public void configureGlobalSecurity(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication()
+                .passwordEncoder(NoOpPasswordEncoder.getInstance()).withUser("admin").password("admin").roles("USER", "ADMIN");
+    }
+    
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/login", "/h2-console/**").permitAll()
+                .antMatchers("/", "/*todo*/**").access("hasRole('USER')").and()
+                .formLogin();
+        
+        http.csrf().disable();
+        http.headers().frameOptions().disable();    }
+}
